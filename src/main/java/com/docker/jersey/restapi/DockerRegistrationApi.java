dabfc6d1feaad149cf66dc1d5ba176f0f75f6f3d
yurabh <yurabh1990@gmail.com>
2022-01-28 13:16:32 +0200

    change statuses http

--- src/main/java/com/docker/jersey/restapi/DockerRegistrationApi.java
+++ src/main/java/com/docker/jersey/restapi/DockerRegistrationApi.java
@@ -8,24 +8,24 @@ import java.util.Optional;
 public class DockerRegistrationApi {
     @POST
     public Response createImage() {
-        return Response.status(201).entity(Optional.empty()).build();
+        return Response.status(501).entity(Optional.empty()).build();
     }
 
     @GET
     @Path("/{id}")
     public Response getImage(@PathParam("id") int id) {
-        return Response.status(201).entity(Optional.empty()).build();
+        return Response.status(501).entity(Optional.empty()).build();
     }
 
     @PUT
     @Path("/{param}")
     public Response updateImage(@PathParam("param") String param) {
-        return Response.status(204).entity(Optional.empty()).build();
+        return Response.status(501).entity(Optional.empty()).build();
     }
 
     @DELETE
     @Path("/{id}")
     public Response deleteImage(@PathParam("id") int id) {
-        return Response.status(202).entity(Optional.empty()).build();
+        return Response.status(501).entity(Optional.empty()).build();
     }
 }
